b0e72967a756b057129b33254652d4f50281cd22
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-02 09:15:23 +0530

    Added:Part time emp wage

--- src/main/java/com/bridgelab/EmpWageCompOOPS.java
+++ src/main/java/com/bridgelab/EmpWageCompOOPS.java
@@ -5,16 +5,26 @@ import java.util.Random;
 public class EmpWageCompOOPS {
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
+
 
     public static void main(String[] args) {
         Random random = new Random();
-        int empCheck = random.nextInt(2);
+        int dailyEmpWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        int empCheck = random.nextInt(3);
         if (empCheck == 0) {
-            System.out.println("Employee is present for work ");
-            int dailyEmpWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee is present");
+            System.out.println("Employee daily wage: "+dailyEmpWage);
+            System.out.println("Employee is present Full day ");
             System.out.println("Employee daily wage: " + dailyEmpWage);
+        } else if (empCheck == 1) {
+            System.out.println("Employee present for Part time");
+            dailyEmpWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+            System.out.println("Employee wage: "+dailyEmpWage);
         } else {
             System.out.println("Employee is Absent");
         }
     }
-    }
+}
+
+
