cfc1d8d8263b83ebd439ee846705a4ee2dfa7168
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-02 08:07:06 +0530

    Added:Employee Attendance

--- src/main/java/com/bridgelab/EmpWageCompOOPS.java
+++ src/main/java/com/bridgelab/EmpWageCompOOPS.java
@@ -1,7 +1,15 @@
 package com.bridgelab;
 
+import java.util.Random;
+
 public class EmpWageCompOOPS {
     public static void main(String[] arg){
-        System.out.println("Welcome to the OOPS EmpWage Program");
-}
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == 0) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is Absent ");
+        }
+    }
 }
