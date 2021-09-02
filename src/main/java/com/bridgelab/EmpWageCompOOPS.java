9acf598bc520bed6c8fe5cebefa588ca352006fb
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-02 08:11:31 +0530

    Added:Program for calculating daily wage

--- src/main/java/com/bridgelab/EmpWageCompOOPS.java
+++ src/main/java/com/bridgelab/EmpWageCompOOPS.java
@@ -3,13 +3,18 @@ package com.bridgelab;
 import java.util.Random;
 
 public class EmpWageCompOOPS {
-    public static void main(String[] arg){
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+
+    public static void main(String[] args) {
         Random random = new Random();
         int empCheck = random.nextInt(2);
         if (empCheck == 0) {
-            System.out.println("Employee is present");
+            System.out.println("Employee is present for work ");
+            int dailyEmpWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee daily wage: " + dailyEmpWage);
         } else {
-            System.out.println("Employee is Absent ");
+            System.out.println("Employee is Absent");
         }
     }
-}
+    }
