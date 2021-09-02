bb4de84c29e2465402a0ee086021cf3fd1d6a5bd
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-02 09:22:13 +0530

    Added:Part time emp wage using switch case

--- src/main/java/com/bridgelab/EmpWageCompOOPS.java
+++ src/main/java/com/bridgelab/EmpWageCompOOPS.java
@@ -11,20 +11,23 @@ public class EmpWageCompOOPS {
     public static void main(String[] args) {
         Random random = new Random();
         int dailyEmpWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-        int empCheck = random.nextInt(3);
-        if (empCheck == 0) {
-            System.out.println("Employee is present");
-            System.out.println("Employee daily wage: "+dailyEmpWage);
-            System.out.println("Employee is present Full day ");
-            System.out.println("Employee daily wage: " + dailyEmpWage);
-        } else if (empCheck == 1) {
-            System.out.println("Employee present for Part time");
-            dailyEmpWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-            System.out.println("Employee wage: "+dailyEmpWage);
-        } else {
-            System.out.println("Employee is Absent");
+        double empCheck = (Math.floor(Math.random() * 10) % 3);
+        switch ((int) empCheck) {
+            case 0:
+                System.out.println("Employee is present for full day ");
+                dailyEmpWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("Employee daily wage: " + dailyEmpWage);
+                break;
+            case 1:
+                System.out.println("Employee present for Part time");
+                dailyEmpWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                System.out.println("Employee wage: " + dailyEmpWage);
+                break;
+            case 2:
+                System.out.println("Employee Absent");
         }
     }
 }
+
 
 
