9231be819a0897eb9d083905057e6a107e5e71bd
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-06 08:49:07 +0530

    Added:Program for Multiple companies

---
+++ src/main/java/com/bridgelab/Company_Info.java
@@ -0,0 +1,70 @@
+package com.bridgelab;
+
+public class Company_Info {
+    private String companyName;
+    private int empRatePerHr;
+    private int noOfWorkingDays;
+    private int maxHrsPerMonth;
+    private int totalWage;
+
+    public Company_Info() {
+    }
+
+    public Company_Info(String companyName, int empRatePerHr, int noOfWorkingDays, int maxHrsPerMonth) {
+        this.companyName = companyName;
+        this.empRatePerHr = empRatePerHr;
+        this.noOfWorkingDays = noOfWorkingDays;
+        this.maxHrsPerMonth = maxHrsPerMonth;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmpRatePerHr() {
+        return empRatePerHr;
+    }
+
+    public void setEmpRatePerHr(int empRatePerHr) {
+        this.empRatePerHr = empRatePerHr;
+    }
+
+    public int getNoOfWorkingDays() {
+        return noOfWorkingDays;
+    }
+
+    public void setNoOfWorkingDays(int noOfWorkingDays) {
+        this.noOfWorkingDays = noOfWorkingDays;
+    }
+
+    public int getMaxHrsPerMonth() {
+        return maxHrsPerMonth;
+    }
+
+    public void setMaxHrsPerMonth(int maxHrsPerMonth) {
+        this.maxHrsPerMonth = maxHrsPerMonth;
+    }
+
+    public int getTotalWage() {
+        return totalWage;
+    }
+
+    public void setTotalWage(int totalWage) {
+        this.totalWage = totalWage;
+    }
+
+  //Override
+    public String toString() {
+        return "CompanyInfo{" +
+                "companyName='" + companyName + '\'' +
+                ", empRatePerHr=" + empRatePerHr +
+                ", noOfWorkingDays=" + noOfWorkingDays +
+                ", maxHrsPerMonth=" + maxHrsPerMonth +
+                ", totalWage=" + totalWage +
+                '}';
+    }
+}
--- src/main/java/com/bridgelab/EmpWageCompOOPS.java
+++ src/main/java/com/bridgelab/EmpWageCompOOPS.java
@@ -1,50 +1,79 @@
 package com.bridgelab;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EmpWageCompOOPS {
-    static final int WAGE_PER_HR = 20;
-    static final int FULL_DAY_HR = 8;
-    static final int PART_TIME_HR = 4;
-    static final int MAX_WORKING_DAYS = 20;
-    static final int MAX_HRS_PER_MONTH = 100;
-    static final int IS_FULL_TIME = 0;
-    static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
 
-    public void employeeWage() {
-        Random random = new Random();
-        int dailyEmpWage;
+    // private static CompanyInfo[] companyInfoArray = new CompanyInfo[3];
+    List<Company_Info> listCompany = new ArrayList<Company_Info>();
+    public static Map<String, Company_Info> companyInfoMap = new HashMap<String, Company_Info>();
+
+    private int numOfCompany = 0;
+
+    public static int computeEmpWage(Company_Info companyInfo) {
+        int empHrs = 0;
         int totalWage = 0;
-        int totalWorkingHrs = 0;
-        int totalWorkingDays = 0;
-        int empHours = 0;
+        int totalHrs = 0;
 
-        while (totalWorkingHrs < MAX_HRS_PER_MONTH && totalWorkingDays < MAX_WORKING_DAYS) {
-            totalWorkingDays++;
-            double empCheck = (Math.floor(Math.random() * 10) % 3);
+        int day = 1;
+        while (day <= companyInfo.getNoOfWorkingDays() && totalHrs < companyInfo.getMaxHrsPerMonth()) {
+            double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck) {
                 case IS_FULL_TIME:
-                    System.out.println("Employee present");
-                    empHours = FULL_DAY_HR;
+                    empHrs = 8;
                     break;
                 case IS_PART_TIME:
-                    System.out.println("Employee present part time");
-                    empHours = PART_TIME_HR;
+                    empHrs = 4;
                     break;
                 default:
-                    System.out.println("Employee Absent on day");
-                    empHours = 0;
+                    empHrs = 0;
             }
-            totalWorkingHrs += empHours;
+            int empWage = empHrs * companyInfo.getEmpRatePerHr();
+
+            totalHrs += empHrs;
+            totalWage += empWage;
+            day++;
         }
-        totalWage = totalWorkingHrs * WAGE_PER_HR;
-        System.out.println("Total Wage: " + totalWage);
-        System.out.println("total working hours: " + totalWorkingHrs);
-        System.out.println("total working Days: " + totalWorkingDays);
+        return totalWage;
     }
 
     public static void main(String[] args) {
-        EmpWageCompOOPS empWage = new EmpWageCompOOPS();
-        empWage.employeeWage();
+        EmpWageCompOOPS employeeWageBuilder = new EmpWageCompOOPS();
+        employeeWageBuilder.addCompanyEmpWage("Dmart", 20, 6, 10);
+        employeeWageBuilder.addCompanyEmpWage("JIO", 27, 16, 10);
+        employeeWageBuilder.addCompanyEmpWage("Airtel", 27, 16, 10);
+
+
+        employeeWageBuilder.computeEmpWage();
+// for (int i = 0; i < companyInfoArray.length; i++) {
+// System.out.println(companyInfoArray[i]);
+// }
+        System.out.println("Map:" + companyInfoMap);
+
+        System.out.println(companyInfoMap.get("Dmart"));
+
     }
+
+    public void addCompanyEmpWage(String companyName, int empRatePerHr, int noOfWorkingDays, int maxHrsPerMonth) {
+        Company_Info companyInfo = new Company_Info(companyName, empRatePerHr, noOfWorkingDays, maxHrsPerMonth);
+// companyInfoArray[numOfCompany] = companyInfo;
+        listCompany.add(companyInfo);
+
+
+    }
+
+    public void computeEmpWage() {
+        for (int i = 0; i < listCompany.size(); i++) {
+            int totalWage = EmpWageCompOOPS.computeEmpWage(listCompany.get(i));
+            listCompany.get(i).setTotalWage(totalWage);
+            companyInfoMap.put(listCompany.get(i).getCompanyName(), listCompany.get(i));
+        }
+
+    }
+
 }
